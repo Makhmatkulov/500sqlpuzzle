71232274574d7fa4a3795f85748a7f36cb229318
Murod <makhmatkulov4828@gmail.com>
2024-08-30 21:04:56 +0500

    Puzzle1 added

---
+++ puzzle1.sql
@@ -0,0 +1,33 @@
+
+--So we first create the table
+ 
+CREATE TABLE lag
+(
+BusinessEntityID INT
+,SalesYear   INT
+,CurrentQuota  DECIMAL(20,4)
+)
+GO
+ 
+INSERT INTO lag
+SELECT 275 , 2005 , '367000.00'
+UNION ALL
+SELECT 275 , 2005 , '556000.00'
+UNION ALL
+SELECT 275 , 2006 , '502000.00'
+UNION ALL
+SELECT 275 , 2006 , '550000.00'
+UNION ALL
+SELECT 275 , 2006 , '1429000.00'
+UNION ALL
+SELECT 275 , 2006 ,  '1324000.00'
+
+
+select * from lag;
+
+
+--solution 1
+select *, 
+       lag(CurrentQuota, 1, 0) over (order by CurrentQuota) as lagCurrentQuota
+from lag;
+
