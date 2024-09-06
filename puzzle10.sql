de3a5e63d37b391509a0eea05a0718cee03f5af4
Murod <makhmatkulov4828@gmail.com>
2024-09-06 18:23:01 +0500

    puzzle 10 added

---
+++ puzzle10.sql
@@ -0,0 +1,75 @@
+--Create Table
+ 
+CREATE TABLE Department
+(
+DeptID INT
+,DeptName VARCHAR(10)
+)
+GO
+ 
+--Insert Data
+ 
+INSERT INTO Department(DeptID,DeptName)
+VALUES
+(1,'Finance'),
+(2,'IT'),
+(3,'HR')
+ 
+--Create Table
+ 
+CREATE TABLE Emps
+(
+EmpID INT
+,EmpName VARCHAR(50)
+,DeptID INT
+,EmpSalary FLOAT
+)
+GO
+ 
+--Insert Data
+ 
+INSERT INTO Emps(EmpID,EmpName,DeptID,EmpSalary) VALUES
+(101,'Isha',1,7000),
+(111,'Esha',1,8970),
+(102,'Mayank',1,8900),
+(103,'Ramesh',2,4000),
+(104,'Avtaar',2,9000),
+(105,'Gopal',3,17000),
+(106,'Krishna',3,1000),
+(107,'Suchita',3,7000),
+(108,'Ranjan',3,17900)
+ 
+--Verify Data
+ 
+  
+SELECT DeptID,DeptName FROM Department;
+SELECT EmpID,EmpName,DeptID,EmpSalary FROM Emps;
+
+
+
+--solution1
+with cte as (
+    select 
+        empid, 
+        empname, 
+        deptid, 
+        empsalary,
+        row_number() over (partition by deptid order by deptid, empsalary) as numing
+    from 
+        emps
+)
+select 
+    cte.empid, 
+    cte.empname, 
+    cte.empsalary, 
+    department.deptname
+from 
+    cte
+join 
+    department 
+on 
+	department.deptid = cte.deptid
+where 
+    numing = 2;
+
+
--- puzzle9.sql
+++ puzzle9.sql
@@ -1,76 +1,29 @@
---Create Table
- 
-CREATE TABLE Department
+--Create table
+CREATE TABLE [dbo].[EmpSalaryGreaterManager]
 (
-DeptID INT
-,DeptName VARCHAR(10)
+[EmpID] [int] NULL,
+[EmpName] [varchar](50) NULL,
+[EmpSalary] [bigint] NULL,
+[MgrID] [int] NULL
 )
 GO
  
 --Insert Data
- 
-INSERT INTO Department(DeptID,DeptName)
+INSERT INTO [EmpSalaryGreaterManager](EmpID,EmpName,EmpSalary,MgrID)
 VALUES
-(1,'Finance'),
-(2,'IT'),
-(3,'HR')
- 
---Create Table
- 
-CREATE TABLE Emps
-(
-EmpID INT
-,EmpName VARCHAR(50)
-,DeptID INT
-,EmpSalary FLOAT
-)
-GO
- 
---Insert Data
- 
-INSERT INTO Emps(EmpID,EmpName,DeptID,EmpSalary) VALUES
-(101,'Isha',1,7000),
-(111,'Esha',1,8970),
-(102,'Mayank',1,8900),
-(103,'Ramesh',2,4000),
-(104,'Avtaar',2,9000),
-(105,'Gopal',3,17000),
-(106,'Krishna',3,1000),
-(107,'Suchita',3,7000),
-(108,'Ranjan',3,17900)
+(1,    'Pawan',      80000, 4),
+(2,    'Dheeraj',    70000, 4),
+(3,    'Isha',       100000,       4),
+(4,    'Joteep',     90000, NULL),
+(5,    'Suchita',    110000,       4)
  
 --Verify Data
- 
-  
-SELECT DeptID,DeptName FROM Department;
-SELECT EmpID,EmpName,DeptID,EmpSalary FROM Emps;
-
+SELECT * FROM [dbo].[EmpSalaryGreaterManager]
 
 
 --solution1
-with cte as (
-    select 
-        empid, 
-        empname, 
-        deptid, 
-        empsalary,
-        row_number() over (partition by deptid order by deptid, empsalary) as numing
-    from 
-        emps
-)
-select 
-    cte.empid, 
-    cte.empname, 
-    cte.empsalary, 
-    department.deptname
-from 
-    cte
-join 
-    department 
-on 
-	department.deptid = cte.deptid
-where 
-    numing = 2;
-
-
-
+select b.EmpID, b.EmpName, b.EmpSalary, b.MgrID
+from EmpSalaryGreaterManager a
+join EmpSalaryGreaterManager b
+on a.EmpID = b.MgrID
+where b.EmpSalary > a.EmpSalary;
